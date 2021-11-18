a6bebfdaf72c1c012cced4f15624f9f77bf37ee8
younghoonkwon <younghoon.kwon91@gmail.com>
2021-11-18 17:20:41 +0900

    refactor: 네이밍 변경 taxi -> dispatch

---
+++ src/main/java/com/test/project/controller/DispatchController.java
@@ -0,0 +1,14 @@
+package com.test.project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dispatch")
+@RequiredArgsConstructor
+public class DispatchController {
+
+
+}
